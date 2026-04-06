1b597db6eafde265a01a3d7c71708f7e7f8dfa5a
Ritesh Kumar <ritesh.k@redbasil.in>
2026-04-06 16:45:34 +0530

    fixed zoom in zoom out issue in android

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
       <body className={inter.className}>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
